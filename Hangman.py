537b39602c272afa3b68afa3f5ee54696530e451
Finn Macrae <Finn.Macrae@academytrainee.com>
2020-02-26 11:53:25 +0000

    Fixed character check on user input

--- Hangman.py
+++ Hangman.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python3.8
-
+import re
 from random import randint
 words = ('apple', 'antelope', 'banana','badger', 'colour','circular', 'dizzy','dither', 'energy','expert', 'flesh','framed', 'grape','giant', 'holiday','hopeful','hymn', 'icicle','irate', 'jazzy','jigsaw', 'knitting','kayaking', 'lumpy','lynx', 'muscle','microwave', 'night','numbskull', 'opened','oxygen','present', 'queen','queue','remove','rhythm','settle','squawk','touch','twelfth','untied','unknown','vases','voodoo','wrench','whiskey','young','yacht','zoomed','zodiac')
+
+
+def charcheck(strg, search=re.compile(r'[^a-z]').search):
+    return not bool(search(strg))
+
 def inputword():
     while True:
-        user_word = (str(input('Player 1: please enter a word for Player 2 to guess: ')))
-    
+        user_word = str((input('Player 1: please enter a word for Player 2 to guess: ')))
+        
         if len(user_word) >= 20:
-           print('That looks too long, please enter a word of 20 characters or less')
+            print('That looks too long, please enter a word of 20 characters or less')
         if len(user_word) <1:
             print("")
-        elif user_word not in 'qwertyuiopasdfghjklzxcvbnm':
-            print('Hey! only enter lowercase letters!')
+        elif charcheck(user_word) == False:
+            print('Please only enter lowercase letters')
         else:
             print('\n' * 50)
             return user_word
